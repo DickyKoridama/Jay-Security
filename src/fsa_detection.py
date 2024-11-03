72d4a6a6533e5ab270f245f07c2754fda1bd30eb
Derek <melzakoridama@gmail.com>
2024-11-03 16:58:04 +0700

    Jay Security 3

--- src/fsa_detection.py
+++ src/fsa_detection.py
@@ -1,3 +1,4 @@
+# src/fsa_detection.py
 class MultiAttackFSA:  
     def __init__(self, normal_threshold, warning_threshold, port_scan_threshold, normal_ips):  
         self.sql_state = 'S0'  
@@ -31,11 +32,8 @@ class MultiAttackFSA:
                 self.ddos_state = 'S1'  
         elif self.ddos_state == 'S1':  
             if self.ddos_request_count > self.warning_threshold:  
-                self.ddos_state = 'S2'    
-        elif self.ddos_state == 'S2':  
+                self.ddos_state = 'S2'  # Deteksi DDoS tercapai
             
-            pass
-        
         print(f"New DDoS state: {self.ddos_state}")
 
     def transition_port_scan(self, ip, scan_count):  
@@ -49,17 +47,17 @@ class MultiAttackFSA:
         
         if self.port_scan_state == 'S0':  
             if self.scan_counts[ip] > self.port_scan_threshold:  
-                self.port_scan_state = 'S1'    
+                self.port_scan_state = 'S1'  # Deteksi port scanning tercapai
         elif self.port_scan_state == 'S1':  
             if self.scan_counts[ip] <= self.port_scan_threshold:  
-                self.port_scan_state = 'S0'    
+                self.port_scan_state = 'S0'  # Kembali ke kondisi normal
                 self.scan_counts[ip] = 0  
 
     def is_sql_attack_detected(self):  
         return self.sql_state == 'S3'  
 
     def is_ddos_attack_detected(self):  
-        return self.ddos_state == 'S2'  
+        return self.ddos_state == 'S2'  # Tidak memerlukan argumen tambahan
 
     def is_port_scan_detected(self):  
         return self.port_scan_state == 'S1'
--- src/utils/fsa_utils.py
+++ src/utils/fsa_utils.py
@@ -1,25 +1,21 @@
+# src/fsa_utils.py
 
+from src.fsa_detection import MultiAttackFSA
 
-def initialize_fsa(normal_threshold, warning_threshold, port_scan_threshold, normal_ips):
-    from fsa_detection import MultiAttackFSA
-    return MultiAttackFSA(normal_threshold, warning_threshold, port_scan_threshold, normal_ips)
+def initialize_fsa(normal_threshold=100, warning_threshold=200, port_scan_threshold=10, normal_ips=None, time_window=10):
+    if normal_ips is None:
+        normal_ips = []
+    return MultiAttackFSA(normal_threshold, warning_threshold, port_scan_threshold, normal_ips, time_window)
 
-def parse_sql_input(sql_input):
-    fsa = initialize_fsa(normal_threshold=100, warning_threshold=200, port_scan_threshold=10, normal_ips=[])
-    for char in sql_input:
-        fsa.transition_sql(char)
-    return fsa.is_sql_attack_detected()
+def parse_sql_input(fsa, ip, sql_input):
+    fsa.transition_sql(ip, sql_input)
+    return fsa.is_sql_attack_detected(ip)
 
-def parse_ddos_requests(request_counts):
-    fsa = initialize_fsa(normal_threshold=100, warning_threshold=200, port_scan_threshold=10, normal_ips=[])
-    for count in request_counts:
-        fsa.transition_ddos(count)
-    return fsa.is_ddos_attack_detected()
+def parse_ddos_requests(fsa, ip, request_count):
+    for _ in range(request_count):
+        fsa.transition_ddos(ip)
+    return fsa.is_ddos_attack_detected(ip)
 
-def parse_port_scan(scan_counts, ip):
-    
-    normal_ips = []  
-    fsa = initialize_fsa(normal_threshold=100, warning_threshold=200, port_scan_threshold=10, normal_ips=normal_ips)
-    for count in scan_counts:
-        fsa.transition_port_scan(ip, count) 
-    return fsa.is_port_scan_detected()
+def parse_port_scan(fsa, ip):
+    fsa.transition_port_scan(ip)
+    return fsa.is_port_scan_detected(ip)
